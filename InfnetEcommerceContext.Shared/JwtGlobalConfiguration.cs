71466d2e667ec50c388c68bd7911603fb57dd07e
Eduardo Rodrigues <edu.ol.ro@gmail.com>
2024-12-31 13:29:54 -0300

    #Add - shared project for jwt configuration

---
+++ InfnetEcommerceContext.Shared/JwtGlobalConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace InfnetEcommerceContext.Shared;
+
+public static class JwtGlobalConfiguration
+{
+    public static IServiceCollection AddJwtTokenConfiguration(this IServiceCollection services, string tokenSecret)
+    {
+        var s = tokenSecret;
+        var key = Encoding.ASCII.GetBytes(s);
+        services.AddAuthentication(x =>
+        {
+            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(x =>
+        {
+            x.RequireHttpsMetadata = false;
+            x.SaveToken = true;
+            x.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        });
+        return services;
+    }
+
+}
